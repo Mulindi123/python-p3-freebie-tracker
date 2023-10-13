da1f9bb7a09445f4cb4e323f88683c21c12290e7
Medrine Mulindi <medrine.mulindi@gmail.com>
2023-10-13 12:04:28 +0300

    Adds Freebie and company-dev_association Models

--- lib/models.py
+++ lib/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
+from sqlalchemy import ForeignKey, Column, Integer, String, MetaData, Table
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,6 +9,14 @@ metadata = MetaData(naming_convention=convention)
 
 Base = declarative_base(metadata=metadata)
 
+company_dev_association = Table(
+    "company_devs",
+    Base.metadata,
+    Column("company_id", Integer, ForeignKey("companies.id"), primary_key=True),
+    Column("dev_id", Integer, ForeignKey("devs.id"), primary_key=True),
+    extend_existing=True
+)
+
 class Company(Base):
     __tablename__ = 'companies'
 
@@ -16,8 +24,14 @@ class Company(Base):
     name = Column(String())
     founding_year = Column(Integer())
 
+    #relationships
+    freebies = relationship("Freebie", backref=backref("company"))
+    devs = relationship("Dev", secondary=company_dev_association, back_populates="devs")
+
     def __repr__(self):
         return f'<Company {self.name}>'
+
+
 
 class Dev(Base):
     __tablename__ = 'devs'
@@ -25,5 +39,20 @@ class Dev(Base):
     id = Column(Integer(), primary_key=True)
     name= Column(String())
 
+    #relationships
+    freebies = relationship("Freebie", backref=backref("dev"))
+    companies = relationship("Company", secondary=company_dev_association, back_populates="devs")
+
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+class Freebie(Base):
+    __tablename__ = "freebies"
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String())
+    value = Column(Integer)
+    company_id = Column(Integer, ForeignKey("companies.id"))
+    dev_id = Column(Integer, ForeignKey("devs.id"))
+
+    def __repr__(self):
+        return f"<Freebie {self.item_name}>"
